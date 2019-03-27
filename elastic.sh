0ae172d1d01edc82b3a7ef8e94a3374d5e9f123e
Ashish Sahu <ashisa@users.noreply.github.com>
2019-03-27 14:22:28 +0530

    Update elastic.sh

--- elastic.sh
+++ elastic.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install apt-transport-https -y
-echo "deb https://artifacts.elastic.co/packages/6.7/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.7.list
-sudo apt-get update -y && sudo apt-get install elasticsearch -y
+#echo "deb https://artifacts.elastic.co/packages/6.7/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.7.list
+#sudo apt-get update -y && sudo apt-get install elasticsearch -y
